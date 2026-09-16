60c345c4ec0f716c5cba593ebdb8a565cac2055e
RRnars <mackevicsrenars@gmail.com>
2026-09-16 15:05:05 +0300

    added ATM menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,50 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+
+        System.out.println("========[ATM]==========");
+
+        Scanner sc  = new Scanner(System.in);
+
+        boolean running = true;
+
+        while(running){
+            System.out.println();
+            System.out.println("1)Check balance");
+            System.out.println("2)Deposit");
+            System.out.println("3)Withdraw");
+            System.out.println("4)Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = sc.nextInt();
+
+            switch (choice){
+                case 1:
+                    System.out.print("Your balance: ");
+
+                case 2:
+                    System.out.print("Enter amount to deposit: ");
+
+                case 3:
+                    System.out.print("Enter amount to withdraw: ");
+
+                case 4:
+
+                    running = false;
+                    System.out.println("Thank you for using this program.");
+                    break;
+
+                    default:
+                        System.out.println("Invalid choice.");
+            }
+
+        }
+
+        sc.close();
+
+
 
 
 
