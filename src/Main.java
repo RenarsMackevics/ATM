7cac9377c48be11af4a8b0c869b993315f266756
RRnars <mackevicsrenars@gmail.com>
2026-09-16 16:07:30 +0300

    merge conflict simulation

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        BankAccount acc = new BankAccount(500.00);
+        BankAccount acc = new BankAccount(500.100);
 
         boolean running = true;
 
